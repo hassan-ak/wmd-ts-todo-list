4a7f58fa6addcd8596934f0ffc05b1b63a69efd0
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-01-12 15:09:09 +0500

    Create display table

---
+++ dist/display/showTodos.js
@@ -0,0 +1,63 @@
+// Display todos List
+import Table from 'cli-table';
+import chalk from 'chalk';
+import { collection, showCompleted } from '../dataModules/collections.js';
+/******************************************/
+// Display todos list
+// if no todo display empty list
+// when showcompleted marked display full list
+// when showcompleted not-marked display masked list
+async function showTodos() {
+    return new Promise((resolve) => {
+        // Initilize table
+        const todosTable = new Table({
+            // Table Heading
+            head: [
+                chalk.blueBright('Id'),
+                chalk.blueBright('Todo'),
+                chalk.blueBright('Completed'),
+            ],
+        });
+        // Check if there are any todos in the storage
+        // If no todos in the list
+        if (collection.getItemCounts().total == 0) {
+            // Title
+            console.log(chalk.bgMagentaBright(`Todos's list (complete)`));
+            todosTable.push(['NA', 'There is no Todo in the List', 'NA']);
+        }
+        // if todos in the list
+        else {
+            // Check for user choice to show all todos
+            // complete and in-complete
+            // If completed list to be displayed
+            if (showCompleted.show) {
+                // Title
+                console.log(chalk.bgMagentaBright(`Todos's list (complete)`));
+                // loop over collection and push entries to the table
+                collection
+                    .getTodoItems(true)
+                    .forEach((item) => todosTable.push([
+                    item.id.toString(),
+                    item.todo,
+                    item.complete ? chalk.green('Yes') : chalk.red('No'),
+                ]));
+            } // list todos after hiding completed
+            else {
+                console.log(chalk.bgMagentaBright(`Todos's list (masked)`));
+                collection
+                    .getTodoItems(false)
+                    .forEach((item) => todosTable.push([
+                    item.id.toString(),
+                    item.todo,
+                    item.complete ? chalk.green('Yes') : chalk.red('No'),
+                ]));
+            }
+        }
+        // Display table
+        console.log(todosTable.toString());
+        // resolve promise
+        resolve(true);
+    });
+}
+/******************************************/
+export { showTodos };
--- stepsToCode/Readme.md
+++ stepsToCode/Readme.md
@@ -393,3 +393,55 @@
   }
   export { toggleDisplay };
   ```
+
+### 8. Create display table
+
+- Create `display/showTodos.ts` to display todos table
+
+  ```ts
+  import Table from 'cli-table';
+  import chalk from 'chalk';
+  import { collection, showCompleted } from '../dataModules/collections.js';
+  async function showTodos(): Promise<true> {
+    return new Promise<true>((resolve) => {
+      const todosTable = new Table({
+        head: [
+          chalk.blueBright('Id'),
+          chalk.blueBright('Todo'),
+          chalk.blueBright('Completed'),
+        ],
+      });
+      if (collection.getItemCounts().total == 0) {
+        console.log(chalk.bgMagentaBright(`Todos's list (complete)`));
+        todosTable.push(['NA', 'There is no Todo in the List', 'NA']);
+      } else {
+        if (showCompleted.show) {
+          console.log(chalk.bgMagentaBright(`Todos's list (complete)`));
+          collection
+            .getTodoItems(true)
+            .forEach((item) =>
+              todosTable.push([
+                item.id.toString(),
+                item.todo,
+                item.complete ? chalk.green('Yes') : chalk.red('No'),
+              ])
+            );
+        } else {
+          console.log(chalk.bgMagentaBright(`Todos's list (masked)`));
+          collection
+            .getTodoItems(false)
+            .forEach((item) =>
+              todosTable.push([
+                item.id.toString(),
+                item.todo,
+                item.complete ? chalk.green('Yes') : chalk.red('No'),
+              ])
+            );
+        }
+      }
+      console.log(todosTable.toString());
+      resolve(true);
+    });
+  }
+  export { showTodos };
+  ```
