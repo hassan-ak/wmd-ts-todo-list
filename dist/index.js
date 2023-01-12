fe765b0275476aaad507db8bc05ac5ea330450d9
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-01-12 16:05:52 +0500

    Call all functions in a sequence

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,48 @@
-console.log('Hello World!');
-export {};
+#!/usr/bin/env node
+// Todos App
+import { title } from './messages/title.js';
+import { shutDown } from './messages/shutDown.js';
+import { addTodo } from './operations/addTodo.js';
+import { showTodos } from './display/showTodos.js';
+import { changeStatus } from './operations/changeStatus.js';
+import { toggleDisplay } from './operations/toggleDisplay.js';
+import { optionSelection } from './userInput/optionSelection.js';
+import { removeCompleted } from './operations/removeCompleted.js';
+/*******************************************/
+// complete todo app
+// call all operations in a sequence
+async function todos() {
+    // loopover check
+    let todosLoop = true;
+    while (todosLoop) {
+        // display title
+        await title();
+        // list todos
+        await showTodos();
+        // user selection to work over
+        let options = await optionSelection();
+        // based on user selection perform operation
+        switch (options) {
+            case 'Add New Todo':
+                await addTodo();
+                break;
+            case 'Show/Hide Completed':
+                await toggleDisplay();
+                break;
+            case 'Change status of Todo':
+                await changeStatus();
+                break;
+            case 'Remove Completed Todo':
+                await removeCompleted();
+                break;
+            case 'Quit':
+                todosLoop = false;
+                await shutDown();
+                break;
+            default:
+                break;
+        }
+    }
+}
+/*******************************************/
+todos();
--- stepsToCode/Readme.md
+++ stepsToCode/Readme.md
@@ -445,3 +445,47 @@
   }
   export { showTodos };
   ```
+
+### 9. Call all functions in a sequence
+
+- Update `index.ts` to combine all the above said functionality
+
+  ```ts
+  import { title } from './messages/title.js';
+  import { shutDown } from './messages/shutDown.js';
+  import { addTodo } from './operations/addTodo.js';
+  import { showTodos } from './display/showTodos.js';
+  import { changeStatus } from './operations/changeStatus.js';
+  import { toggleDisplay } from './operations/toggleDisplay.js';
+  import { optionSelection } from './userInput/optionSelection.js';
+  import { removeCompleted } from './operations/removeCompleted.js';
+  async function todos() {
+    let todosLoop = true;
+    while (todosLoop) {
+      await title();
+      await showTodos();
+      let options = await optionSelection();
+      switch (options) {
+        case 'Add New Todo':
+          await addTodo();
+          break;
+        case 'Show/Hide Completed':
+          await toggleDisplay();
+          break;
+        case 'Change status of Todo':
+          await changeStatus();
+          break;
+        case 'Remove Completed Todo':
+          await removeCompleted();
+          break;
+        case 'Quit':
+          todosLoop = false;
+          await shutDown();
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  todos();
+  ```
